2accd75a911deb3ed5d18cdd23358e5f288b4461
里見健太 <satomikennta1213@gmail.com>
2018-05-24 14:39:58 +0900

    add extension restriction and renaming function

--- routes/identification.js
+++ routes/identification.js
@@ -1,17 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var connection = require('../mysqlConnection');
-var multer = require('multer');
-const upload = multer({
-  dest: './upload/tmp'
-})
+
+const multer = require('multer');
+const multerStorage = multer.diskStorage({
+destination (req, file, cb) {
+cb(null, './upload/tmp');
+},
+filename (req, file, cb) {
+cb(null, req.body.firstname + req.body.lastname + req.session.user_id);
+}
+});
+
+const upload = multer({ storage: multerStorage });
 
 
 //walletの登録
 router.post('/', upload.any(), function(req, res, next) {
     var lastName = req.body.lastname;
     var firstName = req.body.firstname;
-    var idImage = req.body.id_image;
+    var idImage = req.body.firstname + req.body.lastname + String(req.session.user_id);
     var userId = req.session.user_id;
     var savedId = "本人確認を実施しました"
     console.log(req.body)
