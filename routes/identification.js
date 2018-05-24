bd8caaca498d129ba6a5ce91e34d4bdc289895a4
trueno2315 <toueili@gmail.com>
2018-05-24 13:36:15 +0900

    make image uploader function

--- routes/identification.js
+++ routes/identification.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var connection = require('../mysqlConnection');
+var multer = require('multer');
+const upload = multer({
+  dest: './upload/tmp'
+})
+
 
 //walletの登録
-router.post('/', function(req, res, next) {
+router.post('/', upload.any(), function(req, res, next) {
     var lastName = req.body.lastname;
     var firstName = req.body.firstname;
     var idImage = req.body.id_image;
     var userId = req.session.user_id;
     var savedId = "本人確認を実施しました"
+    console.log(req.body)
+    console.log(req.files)
     var updateidQuery  = 'UPDATE users SET firstname = "' + firstName + '",  lastname = "' + lastName + '", id_image= "' + idImage + '" where user_id = ' + userId + '';
     connection.query(updateidQuery, function(err, rows) {
       console.log(updateidQuery);
