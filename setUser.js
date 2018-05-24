47538860f97beffaa7894c1005a4eaad8d308313
trueno2315 <toueili@gmail.com>
2018-05-24 15:13:46 +0900

    modify setuser.js

--- setUser.js
+++ setUser.js
@@ -3,7 +3,7 @@ var connection = require('./mysqlConnection');
 module.exports = function(req, res, next) {
   var userId = req.session.user_id;
   if (userId) {
-    var query = 'SELECT user_id, user_name, wallet FROM users WHERE user_id = ' + userId;
+    var query = 'SELECT user_id, user_name, wallet, kyc FROM users WHERE user_id = ' + userId;
     connection.query(query, function(err, rows) {
       if (!err) {
         res.locals.user = rows.length? rows[0]: false;
