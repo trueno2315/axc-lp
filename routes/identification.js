a694dc8a995603d2a31bf16c854246aaf39e1ac1
里見健太 <satomikennta1213@gmail.com>
2018-07-05 15:31:05 +0900

    add antiReload

--- routes/identification.js
+++ routes/identification.js
@@ -18,21 +18,19 @@ const upload = multer({ storage: multerStorage });
 
 
 //walletの登録
-router.post('/', upload.any(), function(req, res, next) {
+router.post('/', upload.any(), function(req, res, next) { 
     var lastName = req.body.lastname;
     var firstName = req.body.firstname;
     var idImage = req.body.firstname + req.body.lastname + String(req.session.user_id) + myExt;
-    var userId = req.session.user_id;
+    var userId = req.session.user_id; 
     var savedId = "In the process of Identification... Please wait a little"
     var updateidQuery  = 'UPDATE users SET firstname = "' + firstName + '",lastname = "' + lastName + '", id_image= "' + idImage + '",kyc = 1 where user_id = ' + userId + '';
     connection.query(updateidQuery, function(err, rows) {
       console.log(updateidQuery);
       console.log(err);
       console.log(rows);
-        res.render('dashboard', {
-            savedid: savedId
-          });
-    });
+      res.redirect('dashboard');
+    }); 
   });
 
   module.exports = router;
